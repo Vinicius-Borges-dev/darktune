394c82186e0fc78fe9e634a044f2268ae44be054
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-11-21 19:35:02 -0300

    funcionalidades basicas.

--- app/controllers/ContatoController.py
+++ app/controllers/ContatoController.py
@@ -1,19 +1,48 @@
-from flask import current_app as app, request, redirect, url_for, render_template
+from flask import current_app as app, request, redirect, url_for, render_template, flash
 
 
 class ContatoController:
     
     def criar_novo_contato():
-        pass
+        email = request.form.get("email")
+        mensagem = request.form.get("mensagem")
+        
+        try:
+            pass
+        except Exception as erro:
+            app.session.rollback()
+            flash(str(erro))
+            redirect()
     
-    def meus_contatos():
-        pass
+    def meus_contatos(id:int):
+        
+        try:
+            pass
+        except Exception as erro:
+            flash(str(erro))
+            redirect()
     
     def todos_os_contatos():
-        pass
+        
+        try:
+            pass
+        except Exception as erro:
+            flash(str(erro))
+            redirect()
     
-    def atualizar_contato():
-        pass
+    def atualizar_contato(id:int):
+        mensagem = request.form.get("mensagem")
+        
+        try:
+            pass
+        except Exception as erro:
+            flash(str(erro))
+            redirect()
     
-    def deletar_contato():
-        pass
+    def deletar_contato(id:int):
+        
+        try:
+            pass
+        except Exception as erro:
+            flash(str(erro))
+            redirect()
