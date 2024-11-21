41eca339180b773efeae01cd684e4cce3abb2864
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-11-21 19:29:28 -0300

    Criação: definindo os metodos base para a gestão de contatos.

---
+++ app/controllers/ContatoController.py
@@ -0,0 +1,19 @@
+from flask import current_app as app, request, redirect, url_for, render_template
+
+
+class ContatoController:
+    
+    def criar_novo_contato():
+        pass
+    
+    def meus_contatos():
+        pass
+    
+    def todos_os_contatos():
+        pass
+    
+    def atualizar_contato():
+        pass
+    
+    def deletar_contato():
+        pass
