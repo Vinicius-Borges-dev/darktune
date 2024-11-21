2ec19d534b06c098fd56411b6152564dd56f852e
Vinícius-Borges-dev <dev.vinicius.borges@outlook.com>
2024-11-21 15:07:02 -0300

    Criação: Modelos do banco de dados.

---
+++ app/models/UsuariosModel.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String
+from app.configs.Database import Base
+from typing import Dict
+from sqlalchemy.orm import relationship
+
+class UsuariosModel(Base):
+    __tablename__ = "usuarios"
+    
+    id_usuario = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    senha = Column(String(255), nullable=False)
+    
+    curtida = relationship('CurtidasModel')
+    musica = relationship('MusicasModel')
+    
+    def __init__(self, nome:str, email:str, senha:str)->None:
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+    
+    def __repr__(self)->str:
+        return f"<Usuário {self.id_usuario}>"
+    
+    def to_dict(self)->Dict[str,int]:
+        return{
+            "id":self.id_usuario,
+            "nome": self.nome,
+            "email": self.email,
+            "senha": self.senha
+        }
