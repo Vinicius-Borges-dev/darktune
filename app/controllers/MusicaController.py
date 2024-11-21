98dd5ccb2230556c87e00aa6b4b17f43382dbe32
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-11-21 19:43:26 -0300

    base para funcionalidades de músicas.

---
+++ app/controllers/MusicaController.py
@@ -0,0 +1,25 @@
+from flask import current_app as app, request, redirect, flash, url_for
+from app.models import MusicasModel
+
+class MusicaController:
+    
+    def criar_nova_musica():
+        pass
+    
+    def capturar_todas_as_musicas():
+        pass
+    
+    def capturar_musicas_por_nome():
+        pass
+    
+    def capturar_musicas_por_cantor():
+        pass
+    
+    def capturar_musicas_por_categoria():
+        pass
+    
+    def atualizar_musica(id:int):
+        pass
+    
+    def deletar_musica(id:int):
+        pass
