4a41e6c9fe30456d9bffb2b5305404beda8755ae
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-11-22 19:39:20 -0300

    Modificação: Refatoração.

--- app/routes/musicaRoute.py
+++ app/routes/musicaRoute.py
@@ -1,45 +1,44 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash, session
 from app.controllers.MusicaController import MusicaController
 
+musica_bp = Blueprint('musicas', __name__)
 
-musica_bp = Blueprint('musica_bp', __name__)
-
-@musica_bp.route('/musica/cadastro', methods=['POST'])
+@musica_bp.route('/cadastro', methods=['POST'])
 def cadastro_musica():
     return MusicaController.criar_nova_musica()
 
-@musica_bp.route('/musica/editar', methods=['POST'])
+@musica_bp.route('/editar', methods=['POST'])
 def editar_musica():
     pass
 
-@musica_bp.route('/musica/excluir', methods=['POST'])
+@musica_bp.route('/excluir', methods=['POST'])
 def excluir_musica():
     pass
 
-@musica_bp.route('/musica/curtir', methods=['POST'])
+@musica_bp.route('/curtir', methods=['POST'])
 def curtir_musica():
     pass
 
-@musica_bp.route('/musica/descurtir', methods=['POST'])
+@musica_bp.route('/descurtir', methods=['POST'])
 def descurtir_musica():
     pass
 
-@musica_bp.route('/musica/buscar', methods=['POST'])
+@musica_bp.route('/buscar', methods=['POST'])
 def buscar_musica():
     pass
 
-@musica_bp.route('/musica/listar', methods=['GET'])
+@musica_bp.route('/listar', methods=['GET'])
 def listar_musicas():
     pass
 
-@musica_bp.route('/musica/listar/<str:nome_da_musica>', methods=['GET'])
-def listar_musicas_nome(nome_da_musica:str):
+@musica_bp.route('/listar/nome/<string:nome_da_musica>', methods=['GET'])
+def listar_musicas_nome(nome_da_musica: str):
     pass
 
-@musica_bp.route('/musica/listar/<str:categoria>', methods=['GET'])
-def listar_musicas_por_categoria(categoria:str):
+@musica_bp.route('/listar/categoria/<string:categoria>', methods=['GET'])
+def listar_musicas_por_categoria(categoria: str):
     pass
 
-@musica_bp.route('/musica/listar/<str:cantor>', methods=['GET'])
-def listar_musicas_por_cantor(cantor:str):
+@musica_bp.route('/listar/cantor/<string:cantor>', methods=['GET'])
+def listar_musicas_por_cantor(cantor: str):
     pass
