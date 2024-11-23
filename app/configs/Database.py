201371a854ed5e885203ed67053fb1c0cea52fc7
Dev-Vinicius-Borges <dev.vinicius.borges@outlook.com>
2024-11-23 19:00:54 -0300

    Criação de rotas para upload de imagens e audios.

--- app/configs/Database.py
+++ app/configs/Database.py
@@ -20,7 +20,8 @@ class Database:
         Session = sessionmaker(bind=engine)
         
         app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-        app.config['UPLOAD_FOLDER'] = "app/static/uploads"
+        app.config['UPLOAD_IMAGES_FOLDER'] = "app/static/uploads/imagens"
+        app.config['UPLOAD_AUDIOS_FOLDER'] = "app/static/uploads/audios"
         app.config['SESSSION_PERMANENT'] = True
         app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
         app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
