435e5f2d75846242280ab9af3591cab6dc9a6020
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-11-28 18:53:03 -0300

    Novas informações necessárias.

--- app/controllers/CurtidaController.py
+++ app/controllers/CurtidaController.py
@@ -19,6 +19,8 @@ class CurtidaController:
                 app.session.query(
                     CurtidasModel.id_curtida,
                     MusicasModel.nome_musica,
+                    MusicasModel.id_musica,
+                    MusicasModel.url_imagem,
                     CantoresModel.nome_cantor,
                 )
                 .join(
@@ -35,8 +37,28 @@ class CurtidaController:
                 .filter(CurtidasModel.fk_id_usuario == id_usuario)
                 .all()
             )
+            
+            musicas_dict = {}
+            for (
+                id,
+                nome_musica,
+                id_musica,
+                imagem,
+                cantor
+            ) in curtidas:
+                if id not in musicas_dict:
+                    musicas_dict[id] = {
+                        "id":id,
+                        "nome_musica":nome_musica,
+                        "musica_id":id_musica,
+                        "imagem":imagem.split('/')[-1],
+                        "cantores":[]
+                    }
+                musicas_dict[id]['cantores'].append(cantor)
+            
+            lista_musicas = list(musicas_dict.values())
 
-            return curtidas
+            return lista_musicas 
 
         except Exception as erro:
             raise erro
@@ -49,19 +71,16 @@ class CurtidaController:
                 app.session.query(CurtidasModel)
                 .filter(
                     CurtidasModel.fk_id_musica == musica_id,
-                    CurtidasModel.fk_id_usuario == usuario_id
+                    CurtidasModel.fk_id_usuario == usuario_id,
                 )
                 .first()
             )
-            
+
             if curtida_existente:
                 flash("Música ja foi curtida!")
                 return redirect(url_for("paginas.musicas"))
             else:
-                nova_curtida = CurtidasModel(
-                    id_usuario=usuario_id,
-                    id_musica=musica_id
-                )
+                nova_curtida = CurtidasModel(id_usuario=usuario_id, id_musica=musica_id)
 
                 app.session.add(nova_curtida)
                 app.session.commit()
@@ -71,7 +90,11 @@ class CurtidaController:
             raise erro
 
     def descurtir(self, id_curtida: int):
-        curtida = CurtidasModel.query.get(id_curtida)
-
-        app.session.delete(curtida)
-        app.session.commit()
+        try:
+            curtida = app.session.query(CurtidasModel).filter(CurtidasModel.id_curtida == id_curtida).first()
+            app.session.delete(curtida)
+            app.session.commit()
+            
+            return redirect(url_for("paginas.conta"))
+        except Exception as erro:
+            raise erro
