110dd49800e8b6e1a91e69190dff1e0382e0df7f
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-11-28 19:22:32 -0300

    Refatoração.

--- app/routes/musicaRoute.py
+++ app/routes/musicaRoute.py
@@ -1,21 +1,31 @@
 from flask import Blueprint
 from app.controllers.MusicaController import MusicaController
-from app.middlewares.MusicaMiddleware import MusicaMiddleware
 from app.controllers.CurtidaController import CurtidaController
+from app.middlewares.MusicaMiddleware import MusicaMiddleware
+from app.middlewares.UsuarioMiddleware import UsuarioMiddleware
 
 musica_bp = Blueprint('musicas', __name__)
+
+@musica_bp.before_request
+@UsuarioMiddleware.verificar_login
+def before_request():
+    pass
 
 
 @musica_bp.route('/cadastro', methods=['POST'])
 @MusicaMiddleware.checar_existencia_de_musica
+@MusicaMiddleware.checar_formato_imagem
+@MusicaMiddleware.checar_formato_audio
 def cadastro_musica():
     return MusicaController().criar_nova_musica()
 
 @musica_bp.route('/editar', methods=['POST'])
+@UsuarioMiddleware.verificar_permissao_admin
 def editar_musica():
     pass
 
 @musica_bp.route('/excluir/<int:id>', methods=['GET'])
+@UsuarioMiddleware.verificar_permissao_admin
 def excluir_musica(id):
     return MusicaController().deletar_musica(id)
 
@@ -26,10 +36,6 @@ def curtir_musica(id):
 @musica_bp.route('/descurtir/<int:id>', methods=['GET'])
 def descurtir_musica(id):
     return CurtidaController().descurtir(id)
-
-@musica_bp.route('/listar', methods=['GET'])
-def listar_musicas():
-    return MusicaController().capturar_musicas_aleatorias_para_exibicao()
 
 @musica_bp.route('/listar/nome/<string:nome_da_musica>', methods=['GET'])
 def listar_musicas_nome(nome_da_musica: str):
