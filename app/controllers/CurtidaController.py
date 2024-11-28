e4fc67f3e7400957371ed94376dec95bf8534e8a
Vinícius-Borges-dev <dev.vinicius.borges@outlook.com>
2024-11-28 16:28:54 -0300

    Funcionalidade de criação de curtidas.

--- app/controllers/CurtidaController.py
+++ app/controllers/CurtidaController.py
@@ -1,37 +1,59 @@
-from app.models import CurtidasModel, MusicasModel, UsuariosModel, CantoresModel
+from app.models import (
+    CurtidasModel,
+    MusicasModel,
+    UsuariosModel,
+    CantoresModel,
+    CantoresMusicasModel,
+    CategoriasModel,
+)
 from flask import redirect, url_for, session, flash, current_app as app
 
 
 class CurtidaController:
 
     def buscar_minhas_curtidas(self):
-        id_usuario = session["usuario"]
+        try:
+            id_usuario = session["usuario"]
 
-        curtidas = (
-            app.session.query(
-                CurtidasModel.id_curtida,
-                MusicasModel.nome_musica,
-            )
-            .join(MusicasModel, CurtidasModel.fk_id_musica == MusicasModel.id_musica)
-            .filter(CurtidasModel.fk_id_usuario == id_usuario)
-            .all()
-        )
-        
-        for curtida in curtidas:
-            cantores = (
-                app.session.query(CantoresModel)
-                .join(MusicasModel, CantoresModel.id_cantor == MusicasModel.fk_id_cantor)
-                .filter(MusicasModel.nome_musica == curtida.nome_musica)
+            curtidas = (
+                app.session.query(
+                    CurtidasModel.id_curtida,
+                    MusicasModel.nome_musica,
+                    CantoresModel.nome_cantor,
+                )
+                .join(MusicasModel, CurtidasModel.fk_id_musica == MusicasModel.id_musica)
+                .join(
+                    CantoresMusicasModel,
+                    CantoresMusicasModel.fk_id_musica == MusicasModel.id_musica,
+                )
+                .join(
+                    CantoresModel,
+                    CantoresModel.id_cantor == CantoresMusicasModel.fk_id_cantor,
+                )
+                .filter(CurtidasModel.fk_id_usuario == id_usuario)
                 .all()
             )
-            
-            lista_cantores = [cantor.nome_cantor for cantor in cantores]
-            curtida["cantores"] = lista_cantores
-        
-        return curtidas
+
+            return curtidas
+
+        except Exception as erro:
+            raise erro
 
     def criar_nova_curtida(self, id_musica: int):
-        pass
+        try:
+            id_usuario = session["usuario"]
+
+            nova_curtida = CurtidasModel(
+                id_usuario == id_usuario, id_musica == id_musica
+            )
+
+            app.session.add(nova_curtida)
+            app.session.commit()
+
+            flash("Curtida realizada com sucesso!")
+            return redirect(url_for("paginas.musicas"))
+        except Exception as erro:
+            raise erro
 
     def descurtir(self, id_curtida: int):
         curtida = CurtidasModel.query.get(id_curtida)
