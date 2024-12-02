5c8dcef46c4722fcc0f6a407fcb51e74c93fdbd7
Vinícius-Borges-dev <dev.vinicius.borges@outlook.com>
2024-12-02 17:12:32 -0300

    Refatorando estilisações.

--- app/models/CategoriasModel.py
+++ app/models/CategoriasModel.py
@@ -3,22 +3,20 @@ from app.configs.Database import Base
 from typing import Dict
 from sqlalchemy.orm import relationship
 
+
 class CategoriasModel(Base):
-    __tablename__="categorias"
-    
+    __tablename__ = "categorias"
+
     id_categoria = Column(Integer, primary_key=True)
     nome_categoria = Column(String(50), nullable=False)
-    
-    musicas_categorias = relationship('MusicasCategoriasModel')
-    
-    def __init__(self, nome_categoria:str)->None:
+
+    musicas_categorias = relationship("MusicasCategoriasModel")
+
+    def __init__(self, nome_categoria: str) -> None:
         self.nome_categoria = nome_categoria
-    
-    def __repr__(self)->str:
+
+    def __repr__(self) -> str:
         return f"<Categoria: {self.id_categoria}>"
-    
-    def to_dict(self)->Dict[str,int]:
-        return {
-            "id", self.id_categoria,
-            "nome", self.nome_categoria
-        }
+
+    def to_dict(self) -> Dict[str, int]:
+        return {"id", self.id_categoria, "nome", self.nome_categoria}
